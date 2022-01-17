1499436389259cc394af689ab7adfb4411c8a5b8
mcas2 <mcas2@tutanota.com>
2022-01-17 13:11:25 +0100

    InfoSlides avances

---
+++ app/src/main/java/com/rotirmar/lumen/InfoSlide1.java
@@ -0,0 +1,20 @@
+package com.rotirmar.lumen;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InfoSlide1 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_info_slide1, container, false);
+    }
+}
---
+++ app/src/main/java/com/rotirmar/lumen/InfoSlide5.java
@@ -0,0 +1,19 @@
+package com.rotirmar.lumen;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class InfoSlide5 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_info_slide5, container, false);
+    }
+}
