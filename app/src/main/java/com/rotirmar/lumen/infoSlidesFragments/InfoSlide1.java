6b2a9dc17f75942b3bf6ed0e3695324fd7cac851
mcas2 <mcas2@tutanota.com>
2022-01-20 09:23:24 +0100

    Avances con los slides

--- app/src/main/java/com/rotirmar/lumen/infoSlidesFragments/InfoSlide1.java
+++ app/src/main/java/com/rotirmar/lumen/infoSlidesFragments/InfoSlide1.java
@@ -1,4 +1,4 @@
-package com.rotirmar.lumen;
+package com.rotirmar.lumen.infoSlidesFragments;
 
 import android.os.Bundle;
 
@@ -9,6 +9,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.rotirmar.lumen.R;
 
 public class InfoSlide1 extends Fragment {
 
