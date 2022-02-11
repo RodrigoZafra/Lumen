77ec8f6abd0f04447c4b2dfafdd9157b05a23cc2
am3dam2 <rodrifdt17@gmail.com>
2022-02-11 13:49:27 +0100

    arreglando los fragments

--- app/src/main/java/com/rotirmar/lumen/productionFragments/Production1.java
+++ app/src/main/java/com/rotirmar/lumen/productionFragments/Production1.java
@@ -56,7 +56,7 @@ public class Production1 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_consumption1, container, false);
+        return inflater.inflate(R.layout.fragment_production1, container, false);
     }
 
 }
--- app/src/main/java/com/rotirmar/lumen/productionFragments/Production2.java
+++ app/src/main/java/com/rotirmar/lumen/productionFragments/Production2.java
@@ -57,6 +57,6 @@ public class Production2 extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_consumption1, container, false);
+        return inflater.inflate(R.layout.fragment_production2, container, false);
     }
 }
