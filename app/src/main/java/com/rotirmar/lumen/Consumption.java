0093694922b0300a5e001b19910307f9a0d5c176
am3dam2 <rodrifdt17@gmail.com>
2022-02-11 13:06:19 +0100

    arreglando los fragments

--- app/src/main/java/com/rotirmar/lumen/Consumption.java
+++ app/src/main/java/com/rotirmar/lumen/Consumption.java
@@ -35,6 +35,8 @@ public class Consumption extends AppCompatActivity {
         SectionsPagerAdapterConsumption sectionsPagerAdapter = new SectionsPagerAdapterConsumption(this, getSupportFragmentManager());
         ViewPager viewPager = binding.consumptionViewPager;
         viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.consumptionTabs;
+        tabs.setupWithViewPager(viewPager);
 
         BottomNavigationView consumptionBottom = findViewById(R.id.consumption_bottom_navigation);
         consumptionBottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -54,6 +56,25 @@ public class Consumption extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {//Aparece el menu en la barra de arriba
         getMenuInflater().inflate(R.menu.menu_navigation, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {//Gestiona las acciones sobre los botones
+        int id = item.getItemId();
+        if (id == R.id.favorite) {
+            Toast toast = Toast.makeText(this, "Me gusta!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+
+        if (id == R.id.comoFunciona) {
+            Intent intent = new Intent(Consumption.this, InfoSlides.class);
+            startActivity(intent);
+        }
+        if (id == R.id.sobreNosotros) {
+
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
