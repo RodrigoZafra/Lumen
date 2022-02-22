d76818b8106ad0a87e107222b7878fb28d2f7284
am3dam2 <rodrifdt17@gmail.com>
2022-02-22 08:31:57 +0100

    he quitado una gráfica de consumption que era de prueba

--- app/src/main/java/com/rotirmar/lumen/consumptionFragments/Consumption1.java
+++ app/src/main/java/com/rotirmar/lumen/consumptionFragments/Consumption1.java
@@ -30,33 +30,13 @@ import java.util.List;
 
 public class Consumption1 extends Fragment {
     private View view;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_consumption1, container, false);
 
-        AnyChartView pieChart = (AnyChartView) view.findViewById(R.id.graficoGeneracionPorcentual);
-        APIlib.getInstance().setActiveAnyChartView(pieChart);
-        Pie pie = AnyChart.pie();
-
-        List<DataEntry> datat = new ArrayList<>();
-        datat.add(new ValueDataEntry("Hidráulica", 11.1));
-        datat.add(new ValueDataEntry("Eólica", 30));
-        datat.add(new ValueDataEntry("Solar fotovoltaica", 4.4));
-        datat.add(new ValueDataEntry("Solar térmica", 0.5));
-        datat.add(new ValueDataEntry("Otras renovables", 1.9));
-        datat.add(new ValueDataEntry("Residuos renovables", 0.3));
-        datat.add(new ValueDataEntry("Nuclear", 17.3));
-        datat.add(new ValueDataEntry("Turbinación bombeo", 1.2));
-        datat.add(new ValueDataEntry("Ciclo combinado", 19.8));
-        datat.add(new ValueDataEntry("Carbón", 3.2));
-        datat.add(new ValueDataEntry("Cogeneración", 9.6));
-        datat.add(new ValueDataEntry("Residuos no renovables", 0.8));
-
-        pie.data(datat);
-
-        pieChart.setChart(pie);
-
+        
         AnyChartView lineChart = (AnyChartView) view.findViewById(R.id.graficoGeneracionyConsumo);
         APIlib.getInstance().setActiveAnyChartView(lineChart);
         //lineChart.setProgressBar(view.findViewById(R.id.progress_barGeneracionyConsumo));
